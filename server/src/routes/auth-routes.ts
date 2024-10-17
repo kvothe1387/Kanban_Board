d70b0adf1905be0e0c7114ec98b3b8684d67a757
David McCullough <dmccullough488@gmail.com>
2024-10-17 16:50:17 -0500

    Complete login method

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,26 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: { username: req.body.username.toString(), },
+  });
+
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+  const token = jwt.sign({ id: user.id, username }, secretKey, { expiresIn: '1d' });
+
+  return res.json({ token });
+
 };
 
 const router = Router();
